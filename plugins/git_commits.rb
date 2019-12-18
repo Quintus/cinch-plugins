6e49afe80e9cbb4f30a13dcd50f997c21759355a
Marvin Gülker <post+git@guelker.eu>
2019-12-18 17:02:37 +0100

    Handle branches in GitCommits plugin.

--- plugins/git_commits.rb
+++ plugins/git_commits.rb
@@ -10,6 +10,9 @@
 # with Git's hook system. If you want to print a message via Cinch
 # e.g. from a post-receive hook, take a look at the fifo plugin.
 #
+# It is not required to restart the bot in order to regonise new
+# repositories; they will be picked up on the next scan automatically.
+#
 # == Dependencies
 #
 # None except Git.
@@ -51,7 +54,7 @@ class Cinch::GitCommits
   timer 60, :method => :check_new_commits
 
   def on_connect(*)
-    raise(ArgumentError, "No :directory configured")
+    raise(ArgumentError, "No :directory configured") unless config[:directory]
   end
 
   def check_new_commits
@@ -61,35 +64,64 @@ class Cinch::GitCommits
       repositories = Dir.glob("#{config[:directory]}/*.git")
     end
 
-    @heads ||= {}
-    if @heads.empty?
+    @repos ||= {}
+    if @repos.empty?
       repositories.each do |repo|
-        Dir.chdir(repo) do
-          @heads[repo] = `git rev-parse HEAD`.strip
-        end
+        @repos[repo] = read_heads(repo)
       end
     else
       repositories.each do |repo|
-        Dir.chdir(repo) do
-          head = `git rev-parse HEAD`.strip
-          if @heads[repo] != head
-            author, timestamp, subject = `git show -s --format='%an:%at:%s' #{head}`.split(":")
-            commits   = `git log --oneline #{@heads[repo]}..#{head}`.lines.count
-            timestamp = Time.at(timestamp.to_i)
-            reponame  = File.basename(repo).match(/\.git$/).pre_match
+        reponame  = File.basename(repo).match(/\.git$/).pre_match
+        new_heads = read_heads(repo)
 
-            if commits == 1
-              bot.channels.each{|c| c.send("[#{reponame}] One new commit")}
-            else
-              bot.channels.each{|c| c.send("[#{reponame}] #{commits} new commits")}
-            end
+        new_heads.each_pair do |branch, hash|
+          author, timestamp, subject = `git show -s --format='%an:%at:%s' #{hash}`.split(":")
+          timestamp = Time.at(timestamp.to_i)
 
-            bot.channels.each{|c| c.send("[#{reponame}] On #{timestamp.strftime('%Y-%m-%d %H:%M %:z')}, #{author} commited the latest one, #{head[0..6]}: #{subject.lines.first.chomp}")}
-            @heads[repo] = head
+          unless @repos.has_key?(repo)
+            bot.channels.each{|c| c.send("New Git Repository: #{reponame}")}
+            @repos[repo] = {}
           end
+
+          if @repos[repo].has_key?(branch)
+            if @repos[repo][branch] == hash # equal hashes = nothing changed
+              next
+            else # changed hash = new commits
+              commits = `git log --oneline #{@repos[repo][branch]}..#{head}`.lines.count
+
+              if commits == 1
+                bot.channels.each{|c| c.send("[#{reponame}][#{branch}] One new commit")}
+              else
+                bot.channels.each{|c| c.send("[#{reponame}][#{branch}] #{commits} new commits")}
+              end
+            end
+          else
+            bot.channels.each{|c| c.send("[#{reponame}] New branch: #{branch}")}
+          end
+
+          bot.channels.each{|c| c.send("[#{reponame}][#{branch}] On #{timestamp.strftime('%Y-%m-%d %H:%M %:z')}, #{author} commited the latest one, #{head[0..6]}: #{subject.lines.first.chomp}")}
+          @repos[repo] = new_heads
         end
       end
     end
+  end
+
+  private
+
+  def read_heads(repo)
+    heads = {}
+    IO.popen(["git", "-C", repo, "for-each-ref", "--sort=-committerdate", "refs/heads"]) do |io|
+      while line = io.gets
+        ary = line.split(/\s+/)
+        name = ary[2].match(%r<^refs/heads/>).post_match
+        heads[name] = ary[0]
+      end
+    end
+    if (exitstatus = $?.exitstatus) != 0 # Single = intended
+      bot.loggers.warn("Retrieving heads failed for repository #{repo}. Git exitstatus: #{exitstatus}")
+    end
+
+    heads
   end
 
 end
